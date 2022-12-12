2dc483e6df34c66513d4a394fdada1aafc5290f6
lirma04 <109094404+lirma04@users.noreply.github.com>
2022-12-12 21:41:43 +0200

    papildziau komentarais

--- lesson4/exercise.php
+++ lesson4/exercise.php
@@ -5,3 +5,10 @@ declare(strict_types=1);
 
 echo "test1";
 echo "test2";
+
+
+echo "mybranch";
+
+//git checkout master (branch pavadinimas)
+//git checkout -b <<mano pavadinimas> sukurti naujam branchui
+
